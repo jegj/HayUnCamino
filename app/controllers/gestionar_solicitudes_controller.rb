d32af0b61cab9c457bdc18316b025f91f475e03f
Javier Galarza <jegj57@gmail.com>
2013-01-27 20:59:00 -0430

    nuevo cmabios rastreo

--- app/controllers/gestionar_solicitudes_controller.rb
+++ app/controllers/gestionar_solicitudes_controller.rb
@@ -6,5 +6,18 @@ class GestionarSolicitudesController < ApplicationController
 		@titulo="Gestion de Solicitudes"
 		@solicitudes=Solicitudes.all		
 	end
+
+	def modificar
+		@titulo="Modificar Solicitud"
+
+		unless params[:solicitud_id]
+			flash[:error]="Faltan Parametros"
+			redirect_to :action=>"index"
+			return
+		end
+
+		solicitud_id=params[:solicitud_id]
+		@solicitud=Solicitudes.find(solicitud_id)
+	end
 	
 end
